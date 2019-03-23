2142c7a4497242f32dff5121f9ac8952b352b0cf
kevin <kevin.truman@fusi24.com>
2019-03-23 11:48:40 +0700

    update perbaikan

--- src/main/java/com/dev/lms/sintesisapi/controller/ApiController.java
+++ src/main/java/com/dev/lms/sintesisapi/controller/ApiController.java
@@ -2,7 +2,6 @@ package com.dev.lms.sintesisapi.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api")
 public class ApiController {
-	
+
 	@GetMapping("/users/{kode}")
-	List<String> user(@PathVariable String kode){
-		return new ArrayList<String>();
+	Map<String, Object> user(@PathVariable String kode) {
+		Map<String, Object> m = new HashMap<>();
+		m.put("users", new ArrayList<String>());
+		return m;
 	}
-	
+
 	@PostMapping("/users")
 	Map<String, Object> reg() {
 		Map<String, Object> m = new HashMap<>();
 		m.put("status_code", "1");
 		m.put("status", "success");
 		m.put("message", "y");
+		m.put("data", null);
 		return m;
 	}
 
